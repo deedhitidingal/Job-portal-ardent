b0d54864ce5547a4b37a5c29b26d46267158d23e
SOURAV KARMAKAR <98399593+sourav8927@users.noreply.github.com>
2024-08-26 21:07:32 +0530

    User dashboard updated
    Co-authored-by: deedhitidingal <deedhitidingal@users.noreply.github.com>

--- job-portal-client/src/components/User_dash.jsx
+++ job-portal-client/src/components/User_dash.jsx
@@ -1,30 +1,15 @@
-// import React from 'react';
-
-// function User_dash() {
-//   return (
-//     <div className=" h-screen ">
-//       <aside className="w-1/4 bg-blue-500 p-4 h-screen"></aside>
-//       <div className="bg-gray-400 h-24 w-24 rounded-full mx-auto"></div>
-      
-      
-      
-//     </div>
-//   );
-// }
-
-// export default User_dash;
-
-
-
-// src/components/ProfileDashboard.js
 import React from 'react';
+import { CiCirclePlus } from "react-icons/ci";
+import { MdEdit } from "react-icons/md";
 
+{/* <CiCirclePlus className='relative items-center text-center text-6xl justify-center ' /> */}
+{/* <MdEdit /> */}
 function User_dash() {
     return (
-        <div className="flex h-screen mt-20">
-            <aside className="w-1/4 bg-blue-800 p-4">
-                <div className="text-center text-white">
-                    <div className="bg-gray-400 h-24 w-24 rounded-full mx-auto"></div>
+        <div className="flex h-screen ">
+            <aside className="w-1/4 bg-blue-800 p-4 pt-40">
+                <div className="text-center items-center justify-center text-white">
+                    <div className="h-[200px] flex"><img src="../../public/images/new default logo.png" alt="" className="bg-gray-400 h-[200px] w-[200px] rounded-full mx-auto hover:blur-sm " /></div>
                     <p className="mt-4">PROFILE NAME</p>
                     <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mt-4">
                         Edit Profile
@@ -50,10 +35,15 @@ function User_dash() {
                     </nav>
                 </div>
             </aside>
-            <main className="w-3/4 p-8 bg-white">
+            <main className="w-3/4 p-8 bg-white pt-40">
                 <div>
                     <h2 className="text-2xl font-bold mb-4">Dashboard</h2>
+                    <div>
+                    <div className="bg-gray-400 h-24 w-24 rounded-full mx-auto"></div>
+                    <p className="mt-4">PROFILE NAME</p>
+                    </div>
                     <div className="flex">
+                   
                         <div className="w-2/3 bg-gray-100 p-4 rounded">
                             <h3 className="font-bold mb-2">Skills</h3>
                             <p>Details about skills...</p>
